5f6059e1d8c59970acf808f435e35bb3147db57c
Sharan Lao <slao3@mail.sfsu.edu>
2019-02-06 20:28:32 -0800

    FUNCTIONS DEMO

--- node/functions.js
+++ node/functions.js
@@ -1,4 +1,6 @@
 // Declaring with the function keyword
+
+
 function hi() {
   console.log('Say Hello');
 }
@@ -15,5 +17,18 @@ arrow('hi')
 // for practice
 
 // make an arrow function with no block body
+const myArrow = (y) => y + 1;
+//this is the same as the line above:
+// {
+//    return y + 1;
+// }
+console.log(myArrow(8));
+
+
 
 // make a function that returns a function
+const functionFactory = () => () => console.log('This is weird');
+const a = functionFactory(); //a is now the return value of functionFactory which is the arrow function!
+a();
+const anotherFunction = functionFactory;
+anotherFunction()();
